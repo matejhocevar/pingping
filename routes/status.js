ee6384b1ad02a20c63ac2aa7e7ff5909aefc17f1
Matej Hočevar <matej.hocevar@spartan.si>
2018-12-06 12:06:36 +0100

    Improve IP fetching

--- routes/status.js
+++ routes/status.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-const asyncMiddleware = require('../middleware/async');
 const { Location, validate } = require('../models/location');
 
-router.get('/', asyncMiddleware(async function(req, res, next) {
-	let ip = (req.headers != null ? req.headers['x-forwarded-for'] : null) ||
-		req.connection.remoteAddress ||
-		req.socket.remoteAddress ||
-		(req.connection.socket ? req.connection.socket.remoteAddress : null);
-	console.log(`Detected IP: ${ip}`);
+router.get('/', async function(req, res) {
+	try {
+		console.log(req.headers);
+		let ip = (req.headers != null ? req.headers['x-forwarded-for'] : null) ||
+			req.connection.remoteAddress ||
+			req.socket.remoteAddress ||
+			(req.connection.socket ? req.connection.socket.remoteAddress : null);
 
-	let readFromDB = true;
+		let readFromDB = true;
 
-	ip = (ip === '::1') ? '127.0.0.1' : ip;
+		ip = (ip === '::1') ? '89.142.124.173' : ip;
 
-	let object = await Location.findOne({ ip: ip });
+		let object = await Location.findOne({ ip: ip });
 
-	if (!object) {
-		const location = 'Ljubljana';   // Fetch location
-		const { error } = validate({ ip: ip, location });
-		if (error) return res.status(400).send(error.details[0].message);
+		if (!object) {
+			const location = 'Ljubljana';   // Fetch location
+			const { error } = validate({ ip: ip, location });
+			if (error) return res.status(400).send(error.details[0].message);
 
-		object = new Location({ ip: ip, location: location });
-		object = await object.save();
-		readFromDB = false;
+			object = new Location({ ip: ip, location: location });
+			object = await object.save();
+			readFromDB = false;
+		}
+		// res.send(object);
+		res.send({ object: object, 'read_from_db': readFromDB });
 	}
-	// res.send(object);
-	res.send({ object: object, 'read_from_db': readFromDB });
-}));
+	catch (ex) {
+		res.status(400);
+	}
+});
 
 module.exports = router;
